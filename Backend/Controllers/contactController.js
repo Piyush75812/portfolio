b469a14b4d1a17f8f263e7350343ea126c9f1037
Piyush Kumar <piyushkumar63072@gmail.com>
2026-07-08 13:38:51 +0530

    Configure Brevo SMTP

--- Backend/Controllers/contactController.js
+++ Backend/Controllers/contactController.js
@@ -2,17 +2,13 @@ const nodemailer = require("nodemailer");
 
 // Create Transporter
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 60000,
-  greetingTimeout: 60000,
-  socketTimeout: 60000,
 });
 
 // Verify Gmail Connection
